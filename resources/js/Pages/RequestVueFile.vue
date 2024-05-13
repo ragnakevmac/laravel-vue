ed5607ca741969f78d453f71d987bc8d940c83a2
Kevin Macandog <kevin.macandog@jg-corporation.com>
2024-05-13 18:25:30 +0900

    fixed UI and added a styled button

--- resources/js/Pages/RequestVueFile.vue
+++ resources/js/Pages/RequestVueFile.vue
@@ -5,7 +5,11 @@
                 v-model="form.comment"
                 placeholder="Write a comment..."
             ></textarea>
-            <button type="submit">Submit</button>
+            <br>
+            <button type="submit" class="styled-button">Submit</button>
+            <br>
+            <br>
+
         </form>
         <div v-for="request in requests" :key="request.id">
             {{ request.comment }}
@@ -34,3 +38,24 @@ function submit() {
     });
 }
 </script>
+
+<style scoped>
+.styled-button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 8px;
+  transition: background-color 0.3s;
+}
+
+.styled-button:hover {
+  background-color: #45a049;
+}
+</style>
