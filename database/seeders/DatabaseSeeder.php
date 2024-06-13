e827283f37656133ef9f4862171e5aad408b325f
Kevin Macandog <kevin.macandog@jg-corporation.com>
2024-06-13 18:27:58 +0900

    Solved the bug for POST but there is still a bug for UPDATE

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,8 @@ use App\Models\Request;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+#NOTE solved the bug for POST but there's still a bug for UPDATE
+
 class DatabaseSeeder extends Seeder
 {
     /**
